7dc1358bd19dc9bba7f4fca6b666c1759b427d61
Sithumameesha <sithumameesha76@gmail.com>
2022-07-31 09:57:54 +0530

    UI Design

--- src/controller/ServerController.java
+++ src/controller/ServerController.java
@@ -1,4 +1,28 @@
 package controller;
 
-public class ServerController {
+import com.sun.corba.se.spi.activation.Server;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ServerController implements Initializable {
+    public ScrollPane main;
+    public VBox vbox_msgArea;
+    public TextField txtMsg;
+    public Button btnSend;
+
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
